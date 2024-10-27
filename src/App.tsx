18f80d8e65dac83c7f64be8628f2846da7515b6b
DESKTOP-4USMOFH\takuto <uvubantyo@gmail.com>
2024-10-27 23:13:38 +0900

    react-router-domのBrowserRouter as Routerを使ってURLのパスを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Report from './Pages/Report';
 
 function App() {
 	return (
-		<div className="App">
-		
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home/>} />
+				<Route path="/report" element={<Report/>} />
+			</Routes>
+		</Router>
 	);
 }
 
